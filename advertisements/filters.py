6ed18bb39a79e3efc47d6bdd0626bd3c18df50f1
exceptr <98112445+exceptr@users.noreply.github.com>
2022-08-28 20:44:37 +0300

    Initial commit

--- advertisements/filters.py
+++ advertisements/filters.py
@@ -4,9 +4,10 @@ from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
-    """Фильтры для объявлений."""
-
-    # TODO: задайте требуемые фильтры
+    created_at = filters.DateFromToRangeFilter()
+    status = filters.Filter()
+    creator = filters.Filter()
 
     class Meta:
         model = Advertisement
+        fields = ['created_at', 'status', 'creator__id']
--- advertisements/views.py
+++ advertisements/views.py
@@ -1,33 +1,16 @@
-from django_filters import rest_framework as filters
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
 from advertisements.permissions import IsOwner
 from advertisements.serializers import AdvertisementSerializer
-
-
-class AdvertisementCreatorSearchFilter(SearchFilter):
-    search_param = 'creator'
-
-
-class AdvertisementFilter(filters.FilterSet):
-    created_at = filters.DateFromToRangeFilter()
-    status = filters.Filter()
-
-    class Meta:
-        model = Advertisement
-        fields = ['created_at', 'status']
 
 
 class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     permission_classes = [IsAuthenticated, IsOwner]
-    filter_backends = [AdvertisementCreatorSearchFilter, DjangoFilterBackend, SearchFilter]
-    search_fields = ['creator__id']
     filterset_class = AdvertisementFilter
 
     def get_permissions(self):
